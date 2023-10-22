69143c377747ab1b0ef0bda0203158d31a30ceb7
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-10-22 17:53:47 -0300

    feat: add eslint for nextJS

---
+++ generators/next/index.js
@@ -0,0 +1,39 @@
+const Generator = require("yeoman-generator");
+
+module.exports = class extends Generator {
+    installDependencies() {
+    this.spawnCommandSync("pnpm", [
+      "add",
+      "-D",
+      "prettier",
+      "eslint",
+      "eslint-config-prettier",
+      "eslint-plugin-prettier",
+      "eslint-plugin-import",
+      "@vercel/style-guide",
+      "@next/eslint-plugin-next"
+    ]);
+  }
+
+  eslint() {
+    this.fs.copy(this.templatePath(".eslintrc.cjs"), this.destinationPath(".eslintrc.cjs"));
+    this.fs.delete(this.destinationPath(".eslintrc.json"));
+  }
+
+  vscode() {
+    this.fs.copy(this.templatePath(".vscode"), this.destinationPath(".vscode"));
+  }
+
+  eslintIgnore() {
+    this.fs.copy(this.templatePath(".eslintignore"), this.destinationPath(".eslintignore"));
+  }
+
+  editorconfig() {
+    this.fs.copy(this.templatePath(".editorconfig"), this.destinationPath(".editorconfig"));
+  }
+
+  end() {
+    this.log(" ");
+    this.log("Finished, happy coding!");
+  }
+};
